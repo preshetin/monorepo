df134febeeb9d90aa41d10fcfb4a713f5c544331
Petr Reshetin <preshetin@gmail.com>
2025-01-23 17:29:33 +0300

    alembic first migration

--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, Integer, String, Float
 
-DATABASE_URL = "postgresql+asyncpg://user:password@localhost/dbname"
+# DATABASE_URL = "postgresql+asyncpg://user:password@localhost/dbname"
+DATABASE_URL = "postgresql+asyncpg://postgres:mysecretpassword@localhost:5432/wbtgbot"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(
@@ -14,6 +15,17 @@ SessionLocal = sessionmaker(
 Base = declarative_base()
 
 class Product(Base):
+    """
+    Represents a product in the database.
+
+    Attributes:
+        id (int): The primary key of the product.
+        artikul (int): A unique identifier for the product.
+        name (str): The name of the product.
+        price (float): The price of the product.
+        rating (float): The rating of the product.
+        total_quantity (int): The total quantity of the product available.
+    """
     __tablename__ = "products"
 
     id = Column(Integer, primary_key=True, index=True)
