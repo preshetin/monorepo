59a2be7c834a26cad9c1369dc6121824e27a9c11
Petr Reshetin <preshetin@gmail.com>
2025-01-29 20:29:46 +0300

    fix: schedule interval

--- app/scheduler.py
+++ app/scheduler.py
@@ -7,12 +7,9 @@ from .main import fetch_product_data
 scheduler = AsyncIOScheduler()
 
 async def update_product_data(artikul: int, db: AsyncSession):
-    print('starting job for artikul:', artikul)
     product_data = await fetch_product_data(artikul)
-    print('product_data:', product_data)
     if product_data:
         existing_product = await get_product_by_artikul(db, artikul)
-        print('existing_product:', existing_product)
         if existing_product:
             # Update existing product
             existing_product.name = product_data["name"]
@@ -25,7 +22,7 @@ async def update_product_data(artikul: int, db: AsyncSession):
             await create_product(db, product_data)
 
 async def schedule_product_update(artikul: int, db: AsyncSession):
-    scheduler.add_job(update_product_data, IntervalTrigger(minutes=1), args=[artikul, db])
+    scheduler.add_job(update_product_data, IntervalTrigger(minutes=30), args=[artikul, db])
 
 async def start_scheduler():
     scheduler.start()
